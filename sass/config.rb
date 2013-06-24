281411b3aa278fae918b291dcf5af39205b55596
Jack Weinbender <jack.weinbender@gmail.com>
2013-06-24 15:17:56 -0400

    Refactoring without builtin plugins

---
+++ sass/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "../"
+sass_dir = "."
+images_dir = "../images"
+fonts_dir = "../fonts"
+javascripts_dir = "../js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
